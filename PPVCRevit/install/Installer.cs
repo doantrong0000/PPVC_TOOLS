95368b199608d382db96bdb3edeec1a058cab1fa
doantrong0000 <doantrong0000@gmail.com>
2026-03-28 20:31:28 +0700

    Initialization

---
+++ PPVCRevit/install/Installer.cs
@@ -0,0 +1,54 @@
+﻿using Installer;
+using WixSharp;
+using WixSharp.CommonTasks;
+using WixSharp.Controls;
+
+const string outputName = "PPVCRevit";
+const string projectName = "PPVCRevit";
+
+var versioning = Versioning.CreateFromVersionStringAsync(args[0]);
+var project = new Project
+{
+    OutDir = "output",
+    Name = projectName,
+    Platform = Platform.x64,
+    UI = WUI.WixUI_FeatureTree,
+    MajorUpgrade = MajorUpgrade.Default,
+    GUID = new Guid("8E65AC20-35D2-410C-8BB5-496D66067F1E"),
+    BannerImage = @"install\Resources\Icons\BannerImage.png",
+    BackgroundImage = @"install\Resources\Icons\BackgroundImage.png",
+    Version = versioning.VersionPrefix,
+    ControlPanelInfo =
+    {
+        Manufacturer = Environment.UserName,
+        ProductIcon = @"install\Resources\Icons\ShellIcon.ico"
+    }
+};
+
+var wixEntities = Generator.GenerateWixEntities(args[1..]);
+project.RemoveDialogsBetween(NativeDialogs.WelcomeDlg, NativeDialogs.CustomizeDlg);
+
+BuildSingleUserMsi();
+BuildMultiUserUserMsi();
+
+void BuildSingleUserMsi()
+{
+    project.Scope = InstallScope.perUser;
+    project.OutFileName = $"{outputName}-{versioning.Version}-SingleUser";
+    project.Dirs =
+    [
+        new InstallDir(@"%AppDataFolder%\Autodesk\Revit\Addins\", wixEntities)
+    ];
+    project.BuildMsi();
+}
+
+void BuildMultiUserUserMsi()
+{
+    project.Scope = InstallScope.perMachine;
+    project.OutFileName = $"{outputName}-{versioning.Version}-MultiUser";
+    project.Dirs =
+    [
+        new InstallDir(versioning.VersionPrefix.Major >= 2027 ? @"%ProgramFiles%\Autodesk\Revit\Addins" : @"%CommonAppDataFolder%\Autodesk\Revit\Addins", wixEntities)
+    ];
+    project.BuildMsi();
+}
