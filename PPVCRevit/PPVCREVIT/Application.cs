09f9d2f48c8bc5f22dfc175ffe78b1db74640d3c
doantrong0000 <doantrong0000@gmail.com>
2026-03-28 20:32:52 +0700

    up

--- PPVCRevit/PPVCREVIT/Application.cs
+++ PPVCRevit/PPVCREVIT/Application.cs
@@ -1,26 +1,26 @@
-﻿using Nice3point.Revit.Toolkit.External;
-using PPVCREVIT.Commands;
+﻿//using Nice3point.Revit.Toolkit.External;
+//using PPVCREVIT.Commands;
 
-namespace PPVCREVIT
-{
-    /// <summary>
-    ///     Application entry point
-    /// </summary>
-    [UsedImplicitly]
-    public class Application : ExternalApplication
-    {
-        public override void OnStartup()
-        {
-            CreateRibbon();
-        }
+//namespace PPVCREVIT
+//{
+//    /// <summary>
+//    ///     Application entry point
+//    /// </summary>
+//    [UsedImplicitly]
+//    public class Application : ExternalApplication
+//    {
+//        public override void OnStartup()
+//        {
+//            CreateRibbon();
+//        }
 
-        private void CreateRibbon()
-        {
-            var panel = Application.CreatePanel("Commands", "PPVCREVIT");
+//        private void CreateRibbon()
+//        {
+//            var panel = Application.CreatePanel("Commands", "PPVCREVIT");
 
-            panel.AddPushButton<StartupCommand>("Execute")
-                .SetImage("/PPVCREVIT;component/Resources/Icons/RibbonIcon16.png")
-                .SetLargeImage("/PPVCREVIT;component/Resources/Icons/RibbonIcon32.png");
-        }
-    }
-}
+//            panel.AddPushButton<StartupCommand>("Execute")
+//                .SetImage("/PPVCREVIT;component/Resources/Icons/RibbonIcon16.png")
+//                .SetLargeImage("/PPVCREVIT;component/Resources/Icons/RibbonIcon32.png");
+//        }
+//    }
+//}
